485f8071305b965607739472e67bb99ecc6a63fc
gatoquenose <167044521+gatoquenose@users.noreply.github.com>
2024-04-16 08:32:55 -0600

    Update programa1.py

--- programa1.py
+++ programa1.py
@@ -1,6 +1,6 @@
 def cambia(num):
     if num % 10 == 1:
-        return cambia(num // 10), 0
+        return  0 + cambia(num // 10)*10
     else:
         return cambia(num//10) and  num % 10
 print(cambia(101210))
